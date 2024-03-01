5c256237076b651046d6373ab464d458edcb2b77
Aniket Kulkarni <aniket.kulkarni@dremio.com>
2024-03-01 11:30:34 -0500

    Initial commmit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# dremio-python-examples
+# Readme
+
+This repository is a collection of python based examples that connect to and interact with Dremio. The examples support two main ways to connect to Dremio
+
+1. [JDBC driver (legacy)](https://docs.dremio.com/cloud/sonar/client-apps/drivers/jdbc/)
+2. [Apache ADBC driver](https://arrow.apache.org/adbc/main/faq.html#what-exactly-is-adbc)
+  - this uses apache flight
+
+## Prerequisites
+
+1. Python version
+
+Python version >= 3.0 is required to use these examples
+
+2. Libraries
+
+[jaydebeapi](https://pypi.org/project/JayDeBeApi/) and [apache adbc](https://arrow.apache.org/adbc/main/python/driver_manager.html#installation) are the two prerequisites. To install them run - 
+
+```sh
+python3 -m pip install -r requirements.txt
+```
+
+## Connecting to Dremio
+
+### JDBC
+
+1. Download the [JDBC driver (legacy)](https://docs.dremio.com/cloud/sonar/client-apps/drivers/jdbc/) jar. 
+2. Connection example - 
+
+Ensure that either [a PAT](https://docs.dremio.com/cloud/security/authentication/personal-access-token/#creating-a-token) has been generated or [a JWT](https://docs.dremio.com/cloud/security/app-authentication/external-token/) from an external authentication provider is available
+
+```python
+import connection
+import pathlib
+
+params = connection.JdbcConnectionParams(
+    connection.Region.NA, # for North America, or EU for EMEA
+    token = ...., token_type = connection.JdbcConnectionParams.TokenType.PAT, # for PAT
+    project_id = ....,
+    driver_jar = pathlib.Path(....) # path to the jdbc jar file
+)
+
+# optionally other parameters can be set like socks proxy etc.
+params.set_socks_proxy(host, port, ...)
+
+# connect and interact w/ Dremio
+connection = params.connect()
+with connection.cursor() as cursor:
+    cursor.execute(....)
+    for row in cursor:
+        ....
+```
---
+++ connection.py
@@ -0,0 +1,74 @@
+import jaydebeapi
+import adbc_driver_flightsql as adbc
+from adbc_driver_flightsql import dbapi
+
+import typing
+import enum
+import pathlib
+
+class DriverType(enum.Enum):
+    JDBC = 'jdbc:dremio:direct='
+    ADBC = "grpc+tls://"
+
+class Region(enum.Enum):
+    NA = "prod"
+    EU = "prodemea"
+
+class ConnectionParams(dict):
+    def __init__(self, uri: str, **kw):
+        self.uri = uri
+        super().__init__(**kw)
+
+    def connect(self) -> typing.Union[jaydebeapi.Connection, dbapi.Connection]:
+        pass
+
+class JdbcConnectionParams(ConnectionParams):
+    """
+    Connect using dremio legacy JDBC driver
+    """
+    class TokenType(enum.Enum):
+        PAT = "personal_access_token"
+        JWT = "jwt"
+
+    def __init__(self, region: Region, token: str, token_type: TokenType, project_id: str,
+                 driver_jar: pathlib.Path, ssl=True, disableVerification=False):
+        uri = f'{DriverType.JDBC.value}sql.{region.value}.dremio.cloud:443' 
+        if not driver_jar.is_file():
+            raise FileNotFoundError(driver_jar)
+        self.jar = driver_jar
+        super().__init__(uri, username='$token', password=token, ssl=ssl,
+                         disableCertificationVerification=disableVerification,
+                         token_type=token_type.value)
+    
+    def classname(self):
+        return 'com.dremio.jdbc.Driver'
+    
+    def connect(self) -> jaydebeapi.Connection:
+        return jaydebeapi.connect(self.classname(), self.uri, params=self, jars=[self.jar])
+
+    def set_socks_proxy(self, host: str, port: int, username: str=None, password: str=None):
+        self['socksProxyHost'] = host
+        self['socksProxyPort'] = port
+        if username is not None:
+            self['socksProxyUsername'] = username
+            if password is not None:
+                self['socksProxyPassword'] = password
+
+class AdbcConnectionParams(ConnectionParams):
+    """
+    Connect using apache ADBC
+    """
+    def __init__(self, region: Region, pat: str, ssl=True, disableVerification=False):
+        uri = f'{DriverType.ADBC.value}'
+        if not ssl:
+            uri = uri.replace('+tls', '')
+        uri = f'{uri}data.{region.value}.dremio.cloud:443'
+        kw = {
+            adbc.DatabaseOptions.AUTHORIZATION_HEADER.value: f'Bearer {pat}',
+            adbc.DatabaseOptions.TLS_SKIP_VERIFY.value: "false" if disableVerification else "true",
+            f'{adbc.DatabaseOptions.RPC_CALL_HEADER_PREFIX.value}useEncryption': "true" if ssl else "false"
+        }
+        super().__init__(uri, **kw)
+
+    def connect(self) -> dbapi.Connection:
+        return dbapi.connect(self.uri, self)
